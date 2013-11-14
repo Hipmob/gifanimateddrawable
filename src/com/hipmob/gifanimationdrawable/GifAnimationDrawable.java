a19131098f89208b5fd6fdb7ef8a80ba5f782e93
fomojola <oluseyi@alum.mit.edu>
2013-11-14 13:57:34 -0500

    Increased the bufferedinputstream sizes: should make the images load a little faster

--- src/com/hipmob/gifanimationdrawable/GifAnimationDrawable.java
+++ src/com/hipmob/gifanimationdrawable/GifAnimationDrawable.java
@@ -52,15 +52,17 @@ public class GifAnimationDrawable extends AnimationDrawable
 	
 	public GifAnimationDrawable(File f, boolean inline) throws IOException
 	{
-		this(new BufferedInputStream(new FileInputStream(f)), inline);
+	    this(new BufferedInputStream(new FileInputStream(f), 32768), inline);
 	}
 	
 	public GifAnimationDrawable(InputStream is, boolean inline) throws IOException
 	{
 		super();
+		InputStream bis = is;
+		if(!BufferedInputStream.class.isInstance(bis)) bis = new BufferedInputStream(is, 32768);
 		decoded = false;
 		mGifDecoder = new GifDecoder();
-		mGifDecoder.read(is);
+		mGifDecoder.read(bis);
 		mTmpBitmap = mGifDecoder.getFrame(0);
 		android.util.Log.v("GifAnimationDrawable", "===>Lead frame: ["+width+"x"+height+"; "+mGifDecoder.getDelay(0)+";"+mGifDecoder.getLoopCount()+"]");
 		height = mTmpBitmap.getHeight();
